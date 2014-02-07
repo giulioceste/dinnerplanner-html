4d5257da46ac4b914b2c3d99da70c1b0fc95864a
Halit Anil Donmez <halitanil.donmez@gmail.com>
2014-02-07 17:16:05 +0100

    VıewLoader, MaınController
    Added a skeleton Maın Controller
    Added a VıewLoader to standardıze the process of loadıng fxml docs.

---
+++ src/DinnerPlanner/Main.java
@@ -0,0 +1,24 @@
+package DinnerPlanner;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import se.kth.csc.iprog.dinnerplanner.model.DinnerModel;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = new MainView(new DinnerModel(), new MainController());
+        primaryStage.setTitle("Dinner Planner");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
