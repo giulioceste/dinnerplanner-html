2a97a44b5406d300c1cc5341a315199109329565
Sandro Affentranger <asandros@kth.se>
2014-02-14 19:18:56 +0100

    Updated Ingredients View: - has now 5 columns (price column not working yet) - resizes automatically
    Updated Dish View:
    - ingredients table has now also 5 columns (price column not working yet)
    
    Deleted IngredientTableEntry class (is now obsolete)

--- src/se/kth/csc/iprog/dinnerplanner/view/DishView.java
+++ src/se/kth/csc/iprog/dinnerplanner/view/DishView.java
@@ -1,6 +1,5 @@
 package se.kth.csc.iprog.dinnerplanner.view;
 
-import se.kth.csc.iprog.dinnerplanner.model.IngredientTableEntry;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -45,10 +44,17 @@ public class DishView extends BorderPane {
     private TableColumn columnIngredient;
 
     @FXML
+    private TableColumn columnUnit;
+
+    @FXML
     private TableColumn columnQuantity;
 
     @FXML
-    private TableColumn columnCosts;
+    private TableColumn columnPricePerUnit;
+
+    @FXML
+    private TableColumn columnPrice;
+
 
     public DishView(DinnerModel model, Dish selectedDish) {
 
@@ -65,29 +71,29 @@ public class DishView extends BorderPane {
         labelCostsPersons.setText(String.format("$ %s for %d persons",
                 new DecimalFormat("###.##").format(
                         selectedDish.getPrice() * model.getNumberOfGuests()),
-                        model.getNumberOfGuests()));
+                model.getNumberOfGuests()));
         textPreparation.setText("Preparation:\n\n" + selectedDish.getDescription());
         try {
         imageDish.setImage(ImageLoader.load(selectedDish.getImage()));
         } catch (IOException e) {
-            ;
+            throw new RuntimeException("Can't load image " + selectedDish.getImage());
         }
 
         displayTableData(selectedDish.getIngredients());
 
     }
 
-    public void displayTableData(Set<Ingredient> ingrs) {
 
-        ObservableList<IngredientTableEntry> ingredients = FXCollections.observableArrayList();
-        for (Ingredient ingr : ingrs) {
-            ingredients.add(new IngredientTableEntry(ingr));
-        }
-        tableIngredients.setItems(ingredients);
+    public void displayTableData(Set<Ingredient> ingredients) {
 
-        columnIngredient.setCellValueFactory(new PropertyValueFactory<IngredientTableEntry, String>("name"));
-        columnQuantity.setCellValueFactory(new PropertyValueFactory<IngredientTableEntry, Double>("quantity"));
-        columnCosts.setCellValueFactory(new PropertyValueFactory<IngredientTableEntry, Double>("price"));
+        ObservableList<Ingredient> tableItems = FXCollections.observableArrayList(ingredients);
+        tableIngredients.setItems(tableItems);
+
+        columnIngredient.setCellValueFactory(new PropertyValueFactory<Ingredient, String>("name"));
+        columnUnit.setCellValueFactory(new PropertyValueFactory<Ingredient, Double>("unit"));
+        columnQuantity.setCellValueFactory(new PropertyValueFactory<Ingredient, Double>("quantity"));
+        columnPricePerUnit.setCellValueFactory(new PropertyValueFactory<Ingredient, Double>("pricePerUnit"));
+//        columnPrice.setCellValueFactory(new PropertyValueFactory<Ingredient, Double>("price"));
 
     }
 
@@ -99,10 +105,11 @@ public class DishView extends BorderPane {
         assert imageDish != null : "fx:id=\"imageDish\" was not injected: check your FXML file 'DishView.fxml'.";
         assert textPreparation != null : "fx:id=\"textPreparation\" was not injected: check your FXML file 'DishView.fxml'.";
 
-        assert tableIngredients != null : "fx:id=\"tableIngredients\" was not injected: check your FXML file 'DishView.fxml'.";
-        assert columnIngredient != null : "fx:id=\"columnIngredient\" was not injected: check your FXML file 'DishView.fxml'.";
+        assert columnIngredient != null : "fx:id=\"columnIngredients\" was not injected: check your FXML file 'DishView.fxml'.";
+        assert columnUnit != null : "fx:id=\"columnUnit\" was not injected: check your FXML file 'DishView.fxml'.";
         assert columnQuantity != null : "fx:id=\"columnQuantity\" was not injected: check your FXML file 'DishView.fxml'.";
-        assert columnCosts != null : "fx:id=\"columnCosts\" was not injected: check your FXML file 'DishView.fxml'.";
+        assert columnPricePerUnit != null : "fx:id=\"columnPricePerUnit\" was not injected: check your FXML file 'DishView.fxml'.";
+        assert columnPrice != null : "fx:id=\"columnPrice\" was not injected: check your FXML file 'DishView.fxml'.";
 
     }
 
