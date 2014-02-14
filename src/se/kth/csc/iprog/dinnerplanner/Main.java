7a2bf4247e8182f6e2a72156b16f0c0a7c068350
Halit Anil Donmez <halitanil.donmez@gmail.com>
2014-02-14 15:56:02 +0100

    edited the main.css
    added the panel for drag and drop

--- src/se/kth/csc/iprog/dinnerplanner/Main.java
+++ src/se/kth/csc/iprog/dinnerplanner/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = new MainView(new MainController(DinnerModel.getInstance()));
         primaryStage.setTitle("Dinner Planner");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
 
