4d53ebb29c6663aed9a1edc99f4f71e262723882
Giulio Ceste <giuvenk@hotmail.it>
2014-02-25 15:29:40 +0100

    Is this correct?

--- js/view/dishView.js
+++ js/view/dishView.js
@@ -19,6 +19,39 @@ var DishView = function(container, dish, model) {
     //Register an observer to the model
     model.addObserver(this);
 
+
+   var box = $("<div>");
+     // Add all the necessary classes to layout this view correctly.
+     box.style.width="960px";
+
+     // <h1 id="title" class="section-title">Lasagne</h1>
+     var recipeHeading = $("<h1>");
+     recipeHeading.addClass("section-title")
+     recipeHeading.html("Lasagne");
+     box.append(recipeHeading);
+
+     // <div class="row">
+     var recipeBody = $("<div>");
+     recipeBody.addClass("row");
+     recipeBody.appendTo(box);
+
+     // <div id="dishIngredients" class="col-xs-6">
+     var dishDescription = $("<div>");
+     dishDescription.addClass("col-xs-6");
+     dishDescription.attr("id","dishDescription");
+     dishDescription.appendTo(recipeBody);
+
+     // <div id="dishIngredients" class="col-xs-6">
+     var dishIngredients = $("<div>");
+     dishIngredients.addClass("col-xs-6");
+     dishIngredients.attr("id","dishIngredients");
+     dishIngredients.appendTo(recipeBody);
+
+     // <div id="dishPreparation">
+     var dishPreparation = $("<div>");
+     dishPreparation.attr("id","dishPreparation");
+     box.append(dishPreparation);
+
     //This function gets called when there is a change at the model
     this.update = function(arg) {
         var numGuest = model.getNumberOfGuests();
